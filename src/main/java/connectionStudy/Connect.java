83ea8bc6991160a6ae5377539cd5b411852e9739
pedrolopez <pedrolopez@localhost>
2023-09-05 11:27:42 +0200

    creates index and Connect class

---
+++ src/main/java/connectionStudy/Connect.java
@@ -0,0 +1,60 @@
+package connectionStudy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConectaCRUD
+ */
+@WebServlet("/ConectaCRUD")
+public class Connect extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Connect() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String jdbcURL = "jdbc:mysql://localhost:3306/study?useSSL=false";
+		String jdbUser = "root";
+		String jdbcPass = "";
+		String driver = "com.mysql.cj.jdbc.Driver";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.print("Nombre de la BBDD: " + jdbcURL);
+			//Class.forName(driver);
+			Connection connectionStudydb = DriverManager.getConnection(jdbcURL, jdbUser, jdbcPass);
+			out.print("Conectado!");
+			connectionStudydb.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Studyable</title>
+<style type="text/css">
+
+body {
+  font-family: Arial;
+  padding: 20px;
+  background-color: #f5ffd6;
+}
+</style>
+</head>
+<body>
+
+<% response.sendRedirect("itinerary/details"); %>
+
+
+</body>
+</html>
